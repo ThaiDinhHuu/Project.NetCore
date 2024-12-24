ab8642452b2deb2618dc2770f51e272e9ba10d21
ThaiDinhHuu <huunl2002@gmail.com>
2024-12-24 22:48:54 +0700

    Add project files.

---
+++ ProjectDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
